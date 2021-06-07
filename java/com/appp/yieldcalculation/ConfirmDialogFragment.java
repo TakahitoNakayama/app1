49c017c674f251b15738aaadb3690376e4aa7dea
Takahito Nakayama <takahito0826@gmail.com>
2021-06-08 08:54:42 +0900

    oncreateviewメソッドを追記

--- java/com/appp/yieldcalculation/ConfirmDialogFragment.java
+++ java/com/appp/yieldcalculation/ConfirmDialogFragment.java
@@ -5,6 +5,9 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,6 +25,7 @@ public class ConfirmDialogFragment extends DialogFragment {
         builder.setMessage(R.string.dialog_msg);
         builder.setPositiveButton(R.string.dialog_btn_ok, new DialogButtonClickListener());
         builder.setNegativeButton(R.string.dialog_btn_ng, new DialogButtonClickListener());
+        builder.setNegativeButton(R.string.dialog_btn_ng, new DialogButtonClickListener());
         return builder.create();
     }
 
@@ -33,7 +37,7 @@ public class ConfirmDialogFragment extends DialogFragment {
             switch (which) {
                 //「はい」が押された場合、calcメソッドを実行
                 case DialogInterface.BUTTON_POSITIVE:
-                    Calculation.calc();
+                    calc();
                     break;
 
                 //「いいえ」が押された場合、ダイアログを閉じる
@@ -46,46 +50,51 @@ public class ConfirmDialogFragment extends DialogFragment {
         }
     }
 
-    public class Calculation extends AppCompatActivity {
+    @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_blank, container, false);
 
+            public static void calc()
+                {
+                int reserveYear;//積立年数
+                int reserveMoney;//毎月の積立金額
+                double yield;//想定利回り
+                double investmentResults = 0;//運用成績
 
-        public void calc() {
-            int reserveYear;//積立年数
-            int reserveMoney;//毎月の積立金額
-            double yield;//想定利回り
-            double investmentResults = 0;//運用成績
+                EditText number1 = view.findViewById(R.id.Number1);//ナンバー１(積立年数入力欄)を変数にする
+                String number1String = number1.getText().toString();//ナンバー１に入力されたテキストを文字列にして変数に代入
+                reserveYear = Integer.valueOf(number1String);//変数をint型に変える
 
-            EditText number1 = findViewById(R.id.Number1);//ナンバー１(積立年数入力欄)を変数にする
-            String number1String = number1.getText().toString();//ナンバー１に入力されたテキストを文字列にして変数に代入
-            reserveYear = Integer.valueOf(number1String);//変数をint型に変える
+                EditText number2 = view.findViewById(R.id.Number2);//ナンバー２(積立金額入力欄)を変数にする
+                String number2String = number2.getText().toString();//ナンバー２に入力されたテキストを文字列にして変数に代入
+                reserveMoney = Integer.valueOf(number2String);//変数をint型に変える
 
-            EditText number2 = findViewById(R.id.Number2);//ナンバー２(積立金額入力欄)を変数にする
-            String number2String = number2.getText().toString();//ナンバー２に入力されたテキストを文字列にして変数に代入
-            reserveMoney = Integer.valueOf(number2String);//変数をint型に変える
+                EditText number3 = view.findViewById(R.id.Number3);//ナンバー３(想定利回り入力欄)を変数にする
+                String number3String = number3.getText().toString();//ナンバー３に入力されたテキストを文字列にして変数に代入
+                yield = Double.parseDouble(number3String);//変数をdouble型に変える
 
-            EditText number3 = findViewById(R.id.Number3);//ナンバー３(想定利回り入力欄)を変数にする
-            String number3String = number3.getText().toString();//ナンバー３に入力されたテキストを文字列にして変数に代入
-            yield = Double.parseDouble(number3String);//変数をdouble型に変える
+                double monthYield = (yield / 12) / 100 + 1;//月間利回りを求める
+                Log.d("Main", "" + monthYield);
 
-            double monthYield = (yield / 12) / 100 + 1;//月間利回りを求める
-            Log.d("Main", "" + monthYield);
+                int reserveMonth = reserveYear * 12;//入力された年数を月数に変換
+                Log.d("Main", "" + reserveMonth);
 
-            int reserveMonth = reserveYear * 12;//入力された年数を月数に変換
-            Log.d("Main", "" + reserveMonth);
+                for (int i = 1; i <= reserveMonth; i++) {
+                    if (i == 1) {
+                        investmentResults = reserveMoney;
+                    } else {
+                        investmentResults += ((long) investmentResults) * monthYield + reserveMoney - (long) investmentResults;
+                    }
+                    Log.d("Main", "" + (int) investmentResults);
 
-            for (int i = 1; i <= reserveMonth; i++) {
-                if (i == 1) {
-                    investmentResults = reserveMoney;
-                } else {
-                    investmentResults += ((long) investmentResults) * monthYield + reserveMoney - (long) investmentResults;
                 }
-                Log.d("Main", "" + (int) investmentResults);
-
+                int Results = (int) investmentResults;//運用成績をint型に変換
+                String str = String.valueOf(Results);//運用成績を文字列型に変換
+                TextView textView = view.findViewById(R.id.textViewresult);
+                textView.setText(str);//運用成績をテキストビューに出力
             }
-            int Results = (int) investmentResults;//運用成績をint型に変換
-            String str = String.valueOf(Results);//運用成績を文字列型に変換
-            TextView textView = findViewById(R.id.textViewresult);
-            textView.setText(str);//運用成績をテキストビューに出力
+            return view;
         }
     }
 }
